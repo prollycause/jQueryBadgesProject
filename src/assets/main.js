d0bfea9d66c073afe6b67ccab0972b495b55ca1a
James Paradise <James Paradise>
2017-03-03 09:37:12 -0500

    update

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,8 +1,37 @@
 $(function() {
 
-var url = "https://www.codeschool.com/users/lowtyde.json";
+$.ajax({
+	url: 'https://www.codeschool.com/users/lowtyde.json',
+	dataType: 'jsonp',
+	success: function(response) {
+		addCourses(response.courses.completed);
+	}
+});
 
-console.log(url)
+function addCourses(courses) {
+	var $badges = $('#badges');
 
+	courses.forEach(function(course) {
+
+		var $course = $('<div />', {
+			'class' : 'course'
+		}).appendTo($badges);
+
+		$('<h3 />', {
+			text: course.title
+		}).appendTo($course);
+
+		$('<img />', {
+			src: course.badge
+		}).appendTo($course);
+
+		$('<a />', {
+			'class': 'btn btn-primary',
+			target: '_blank', 
+			href: course.url,
+			text: 'See Course'
+		}).appendTo($course);
+	})
+}
 
 });
